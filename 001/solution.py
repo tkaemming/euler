7d556e27b16f1fde2cc801a41599aa48b1464dbd
Ted Kaemming <ted@kaemming.com>
2012-02-20 20:36:05 -0800

    refactor python solution to 0001

--- 001/solution.py
+++ 001/solution.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
-import argparse, functools, itertools
+import argparse
+from itertools import ifilter
+from functools import partial
 
 def divisible(denominator, numerator):
     return (numerator % denominator) is 0
 
+def keep(predicates, value):
+    return any((predicate(value) for predicate in predicates))
+
+def filter_divisible(denominators, sequence):
+    """
+    Filters all the numbers out of `sequence` that are divisible by any of `denominators`.
+    """
+    predicates = [partial(divisible, denominator) for denominator in denominators]
+    return ifilter(partial(keep, predicates), sequence)
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="prints the sum of all "
+        "integers divisible by denominators up to the provided maximum integer")
     parser.add_argument('maximum', metavar='max', type=int)
     parser.add_argument('denominators', metavar='denominator', type=int, nargs='+')
     options = parser.parse_args()
-
-    tests = [functools.partial(divisible, denominator) for denominator in options.denominators]
-    def keep(number):
-        return any((test(number) for test in tests))
-
-    print sum(itertools.ifilter(keep, xrange(1, options.maximum)))
+    print sum(filter_divisible(options.denominators, xrange(1, options.maximum)))
