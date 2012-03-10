8387e071f22d1c2dc7c7340b716bfe307cfb2279
Ted Kaemming <ted@kaemming.com>
2012-03-09 22:39:54 -0800

    use a locally bound function for filter predicate

--- 001/solution.clj
+++ 001/solution.clj
@@ -1,4 +1,5 @@
 (defn sum [coll] (apply + coll))
 (defn divides-evenly [numerator denominator] (= 0 (mod numerator denominator)))
-(defn predicate [num] (some true? (map #(divides-evenly num %) [3 5])))
-(sum (filter predicate (range 1000)))
+(letfn [(predicate [num]
+          (some true? (map #(divides-evenly num %) [3 5])))]
+  (sum (filter predicate (range 1000))))
