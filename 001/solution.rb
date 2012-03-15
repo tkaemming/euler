8da3d0798df6447a8f76f91280d67f539248551d
Ted Kaemming <ted@kaemming.com>
2012-03-14 19:36:41 -0700

    variable args for max value, denominators

--- 001/solution.rb
+++ 001/solution.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
-max = 1000
-denominators = [3, 5]
+max = ARGV.fetch(0, 1000).to_i
+denominators = ARGV.drop(1).collect { |i| i.to_i }
+if denominators.empty?
+  denominators = [3, 5]
+end
 
 puts (1...max).select { |numerator|
   denominators.any? { |denominator| numerator % denominator == 0 }
