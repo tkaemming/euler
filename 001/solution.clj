09e9383dbe73b59de6122efc367829f59dc6cc69
Ted Kaemming <ted@kaemming.com>
2012-03-14 19:27:30 -0700

    actually print result from clojure

--- 001/solution.clj
+++ 001/solution.clj
@@ -2,4 +2,4 @@
 (defn divides-evenly [numerator denominator] (= 0 (mod numerator denominator)))
 (letfn [(predicate [num]
           (some true? (map #(divides-evenly num %) [3 5])))]
-  (sum (filter predicate (range 1000))))
+  (println (sum (filter predicate (range 1000)))))
