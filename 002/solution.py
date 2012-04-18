87b8efefed37a45e936a4b9339d1bb8d4319a04c
Ted Kaemming <ted@kaemming.com>
2012-04-17 22:47:04 -0700

    python solution to #2

---
+++ 002/solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from itertools import ifilter, takewhile
+
+def fibonacci(start=1):
+    """Generates an infinite length Fibonacci sequence."""
+    a = start
+    yield a
+    b = a + a
+    yield b
+    while True:
+        c = a + b
+        yield c
+        a = b
+        b = c
+
+def is_even(n):
+    """Returns True if a number is even."""
+    return n % 2 == 0
+
+maximum = 4 * 1e6
+print sum(takewhile(lambda x: x < maximum, ifilter(is_even, fibonacci())))
